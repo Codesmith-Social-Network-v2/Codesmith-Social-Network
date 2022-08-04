3bebd4f85257d2aaf98c3b7a24353506d987e475
Lurkbot9000 <guidry.brett@gmail.com>
2022-08-04 16:25:46 -0500

    update to fill empty resident photos

--- src/components/ResidentBox.jsx
+++ src/components/ResidentBox.jsx
@@ -4,9 +4,11 @@ export const ResidentBox = (props) => {
 //<a href>
 
   return(
+   
+
     <button className="ResidentBox" id={props.id} onClick={() => parent.open(props.linkedin)}>
       { !props.photo
-        ? <div className="EmptyPhoto">404<br/>Not Found</div>
+        ? <div className="EmptyPhoto"><img src='https://picsum.photos/200' /> {console.log('ResidenBox.jsx' )}</div>
         : <img className="ProfilePicture"src={props.photo} />
       }
       <div className='cardInfo'>{props.name}</div>
